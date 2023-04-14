868f9c486ab5c7c38b37783f92a9c1c4de894b3f
bangank36 <bangank36@gmail.com>
2023-04-14 12:47:23 +0700

    - Update base style

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var plumber      = require('gulp-plumber');
 var rename       = require('gulp-rename');
 var sass = require('gulp-sass')(require('sass'));
 var sourcemaps   = require('gulp-sourcemaps');
+var merge = require('merge-stream');
 
 var onError = function(err) {
   notify.onError({
@@ -35,22 +36,31 @@ gulp.task('sass', function() {
 });
 
 gulp.task('dist:css', function() {
-  return gulp.src('_sass/hamburgers/hamburgers.scss')
-    .pipe(sass())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest('dist'))
-    .pipe(csscomb('.csscomb.dist.json'))
-    .pipe(cssnano())
-    .pipe(rename('hamburgers.min.css'))
-    .pipe(gulp.dest('dist'));
+  var types = [    '3dx',    '3dx-r',    '3dy',    '3dy-r',    '3dxy',    '3dxy-r',    'arrow',    'arrow-r',    'arrowalt',    'arrowalt-r',    'arrowturn',    'arrowturn-r',    'boring',    'collapse',    'collapse-r',    'elastic',    'elastic-r',    'emphatic',    'emphatic-r',    'minus',    'slider',    'slider-r',    'spin',    'spin-r',    'spring',    'spring-r',    'stand',    'stand-r',    'squeeze',    'vortex',    'vortex-r'  ];
+
+  var streams = types.map(function(type) {
+    return gulp.src('_sass/hamburgers/hamburgers.scss')
+      .pipe(sass({
+        data: '@import "types/' + type + '";'
+      }))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(gulp.dest('dist'))
+      .pipe(csscomb('.csscomb.dist.json'))
+      .pipe(cssnano())
+      .pipe(rename('hamburgers-' + type + '.min.css'))
+      .pipe(gulp.dest('dist'));
+  });
+
+  return merge(streams);
 });
+
 
 gulp.task('watch', function() {
   var browserSyncConfig = require('./bs-config.js');
 
   browserSync.init(browserSyncConfig);
 
-  gulp.watch('_sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('_sass/**/*.scss', gulp.series('dist:css'));
 });
 
 gulp.task('build', gulp.series('sass'));
@@ -58,3 +68,4 @@ gulp.task('build', gulp.series('sass'));
 gulp.task('dist', gulp.series('dist:css'));
 
 gulp.task('default', gulp.series('build', 'watch'));
+/* Find the URL at http://localhost:3001/dist/example.html */
